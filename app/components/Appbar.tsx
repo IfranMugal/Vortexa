70e515eaa4028f4d4d3e9e20e16f2264408c8e3f
devanshu0x <devanshudotdev@gmail.com>
2025-09-24 13:06:37 +0530

    adjusted for hot reload

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,45 +1,17 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
 import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
 
 export function Appbar() {
   const session = useSession();
 
   return (
-    <div className="flex justify-between items-center px-6 py-3 bg-gradient-to-r from-purple-700 via-indigo-500 to-blue-600 shadow-lg rounded-b-2xl">
-      {/* Rotating Logo + Heading */}
-      <div className="flex items-center gap-3">
-        <motion.div
-          animate={{ rotate: 360 }}
-          transition={{ repeat: Infinity, duration: 6, ease: "linear" }}
-          className="text-3xl"
-        >
-          ⚡
-        </motion.div>
-        <h1 className="font-extrabold text-3xl tracking-wide text-white drop-shadow-lg">
-          Vortexa
-        </h1>
+    <div className="bg-green-200 border-b border-green-300  px-4 py-2 mb-2">
+      <div className="flex">
+        <h1>Fasal</h1>
       </div>
 
-      {/* Auth Buttons */}
-      <div>
-        {session.data?.user && (
-          <button
-            onClick={() => signOut()}
-            className="px-4 py-2 bg-white/20 text-white font-medium rounded-full border border-white/30 hover:bg-white/30 transition-colors backdrop-blur-sm"
-          >
-            Sign Out
-          </button>
-        )}
-        {!session.data?.user && (
-          <button
-            onClick={() => signIn()}
-            className="px-4 py-2 bg-white/20 text-white font-medium rounded-full border border-white/30 hover:bg-white/30 transition-colors backdrop-blur-sm"
-          >
-            Sign In
-          </button>
-        )}
-      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center p-4">
-      <Appbar />
       <Link
         href="/dashboard"
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
